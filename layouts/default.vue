93e00d53ebd0a49d15367c6d27338b3d5baaec7e
patrickjparker <patrickjpark@gmail.com>
2023-12-08 15:22:01 -0700

    Make page take full width unless the screen is too small

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="h-screen flex flex-col">
     <NavBar />
     <main class="flex w-full justify-center bg-slate-100 flex-grow">
-      <div class="max-w-[50rem] bg-white">
+      <div class="w-[50rem] max-w-full bg-white">
         <slot />
       </div>
     </main>
