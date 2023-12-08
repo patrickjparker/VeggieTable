d5769c14e8b1ec81ed12eb8b0f4b2591dfe77083
patrickjparker <patrickjpark@gmail.com>
2023-12-08 15:22:16 -0700

    Add a article not found page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -5,6 +5,13 @@
 
 <template>
   <div class="p-16">
-    <ContentDoc />
+    <ContentDoc>
+      <template #not-found>
+        <div class="flex flex-col items-center">
+          <ProseH1 class="inline">Sorry!</ProseH1>
+          <p>There is no page at this address.</p>
+        </div>
+      </template>
+    </ContentDoc>
   </div>
 </template>
